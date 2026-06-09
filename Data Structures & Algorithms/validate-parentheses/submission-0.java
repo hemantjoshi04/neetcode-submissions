c009174e2dbab28d0dee5cd2bcfba1e0ebdd442c
Hemant Joshi <1hemantg@gmail.com>
2026-06-10 00:01:38 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c==']'||c=='}'||c==')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.pop();
+                if((c==']'&&top!='[')||(c=='}'&&top!='{')||(c==')'&&top!='(')){
+                    return false;
+                }
+            }
+            else {stack.push(c);}
+        }
+        return stack.isEmpty()?true:false;
+        
+    }
+}
